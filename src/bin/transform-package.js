383a89d0f5d1cc656c407fe51c5afbbccf742c06
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-10 13:52:33 -0700

    features

--- src/bin/transform-package.js
+++ src/bin/transform-package.js
@@ -5,22 +5,31 @@ import { assert } from 'chai'
 import deasync from 'deasync'
 import transformPackage from '../'
 
-const usage = (actual, err) => `usage: transform <path/to/transform/dir> <package.json> | you passed ${JSON.stringify(actual)} | err: ${err}`
 
+const usage = actual => `usage: transform-package <path/to/transform/dir> <package.json> | you passed ${JSON.stringify(actual)}`
 const args = argv.__
-assert.ok(args, 'translate requires arguments')
-assert(args.length === 2, usage(args))
+const handleError = err => {
+  if(err)
+    console.error(err, usage(args))
+  else
+    console.error(usage(args))
+  process.exit(1)
+}
+
+if(!args)
+  handleError(new Error('No args passed.'))
+if(args.length > 2)
+  handleError(new Error('Too many args passed.'))
 
 const [packageTransformDir, packagePath]  = args
 let done = false
-
-transformPackage(packageTransformDir, packagePath)
+transformPackage(packageTransformDir, packagePath || 'package.json')
   .then(message => {
     console.info(message)
     done = true
   })
   .catch(err => {
-    console.error(usage(args, err))
+    console.error(handleError(err))
     done = true
   })
 deasync.loopWhile(() => !done)
