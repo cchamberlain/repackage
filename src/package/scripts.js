81ec22d9636363bb0825ce610de3f53824f58910
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-28 11:05:16 -0700

    updated build scripts

--- src/package/scripts.js
+++ src/package/scripts.js
@@ -1,15 +1,21 @@
+const target = ['src', '.']
 
-export default ({}) => ({ 'repackage': 'node bin/repackage -t src/package -p package.json'
-                        , 'prebuild': 'rimraf bin && rimraf lib'
-                        , 'build': 'babel src -d .'
-                        , 'watch': 'npm run build -- --watch'
-                        , 'predoc': 'rimraf doc'
-                        , 'doc': 'esdoc -c ./esdoc.json'
-                        , 'prerelease': 'npm run build'
-                        , 'release': 'npm version patch && npm publish'
-                        , 'postrelease': 'npm run release-doc'
-                        , 'prerelease-doc': 'npm run doc'
-                        , 'release-doc': 'git subtree push --prefix doc origin gh-pages'
-                        , 'postrelease-doc': 'git commit -am \"doc-release\" && git push --follow-tags'
-                        , 'test': 'karma start'
-                        })
+const clean = (targets = []) => targets.map(x => `rimraf ${x}`).join(' && ')
+const compile = (targets = []) => targets.map(([src, dest, { isDir = true, watch = false } = {}]) => `babel ${src} ${isDir ? '-d' : '-o'}${watch ? ' --watch' : ''}`).join(' && ')
+
+export default ({} = {}) => ( { 'clean': clean(['bin', 'lib'])
+                              , 'prestart': 'npm run clean'
+                              , 'start': compile([[...target, { watch: true }]])
+                              , 'prebuild': 'npm run clean'
+                              , 'build': compile([target])
+                              , 'predoc': clean(['doc'])
+                              , 'doc': 'esdoc -c ./esdoc.json'
+                              , 'prerelease': 'npm run build'
+                              , 'release': 'npm version patch && npm publish'
+                              , 'postrelease': 'npm run release-doc'
+                              , 'prerelease-doc': 'npm run doc'
+                              , 'release-doc': 'git subtree push --prefix doc origin gh-pages'
+                              , 'postrelease-doc': 'git commit -am \"doc-release\" && git push --follow-tags'
+                              , 'upgrade': 'ncu -a && npm update'
+                              , 'test': 'karma start'
+                              } )
